c1a83db585bfb8a534d107bd9a223e5b1e503318
firoz.nur <firoznur5@gmail.com>
2025-06-05 14:42:08 +0600

    Get : NextAuth Session Data

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,34 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   session: {
     strategy: "jwt",
   },
-  providers:{
-
-  },
-  pages:{
-    
-  }
+  providers: [
+    Credentials({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          required: true,
+          placeholder: "You email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          required: true,
+          placeholder: "Enter Password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+        return null;
+      },
+    }),
+  ],
 });
 
 export { handler as GET, handler as POST };
